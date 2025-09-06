bfb7f09c4de1e32c9681a18cd3e5ee87e49526a0
Zayaan <zayaanra@buffalo.edu>
2025-09-05 22:13:30 -0400

    added gemini llm q/a and session storage in redis

--- backend/app/llm/llm_factory.py
+++ backend/app/llm/llm_factory.py
@@ -9,18 +9,16 @@ GEMINI_API_KEY = os.environ["GEMINI_API_KEY"]
 
 class GenAI():
     def __init__(self, model: str = "gemini-2.5-flash"):
-        self.model = model
-        
         self.client = genai.Client(api_key=GEMINI_API_KEY)
+        self.model = model
     
-    def ask(self, question: str, context: str) -> str:
+    def query(self, context: str, question: str) -> str:
         response = self.client.models.generate_content(
             model=self.model,
             contents=[
-                "Please analyze the following XML file:",
-                context,
-                question
+                {"role": "user", "parts": [{"text": context}]},
+                {"role": "user", "parts": [{"text": question}]}
             ]
         )
 
-        print(response.text)
+        return response.text
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,16 @@
 import uvicorn
 import logging
+import secrets
+import redis
+
+from fastapi import FastAPI, Cookie
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from repository.schemas import *
 from repository.repository import *
 from repository.file import *
-
 from llm.llm_factory import GenAI
-from llm.prompts import *
-
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from fastapi.middleware.cors import CORSMiddleware
 
 from dotenv import load_dotenv
 load_dotenv(override=True)
@@ -26,8 +26,9 @@ for noisy_logger in [
     "httpx",
     "asyncio",
     "starlette",
-    "fastapi"
-    "urllib3.connectionpool"
+    "fastapi",
+    "urllib3.connectionpool",
+    "watchfiles.main"
 ]:
     logging.getLogger(noisy_logger).setLevel(logging.WARNING)
 
@@ -36,43 +37,60 @@ logger = logging.getLogger(__name__)
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
 
-llm = GenAI()
+redis_client = redis.Redis(host="localhost", port=6379, decode_responses=True)
+llm_client = GenAI()
 
-@app.get("/")
-async def read_root() -> dict:
-    return {"Hello": "World"}
-
-@app.post("/submit")
-def submit_repo(r: Repo):
+@app.post("/connect")
+def connect_to_repo(r: Repo):
     repository = Repository(r.owner, r.name)
     status_code = repository.download()
     repository_info = repository.convert_to_json()
 
-    if status_code == 200:
-        return JSONResponse(content={
-            "message": f"Connected to repository: {repository.name}", 
-            "repository_info": repository_info, 
-            }, 
-            status_code=status_code
-        )
-    else:
-        return JSONResponse(content={
-            "message": f"Failed to find repository: {repository.name}. Please try again."}, 
-            status_code=status_code
-        )
-    
-@app.post("/ask")
-def ask(q: Question):
-    question = Question(q.question)
-    llm.ask(question, context="")
+    session_id = secrets.token_urlsafe(16)
+    redis_client.set(session_id, json.dumps(repository_info))
 
-    # TODO - maintain session somehow so we know which repo is being asked about
+    if status_code == 200:
+        response = JSONResponse(content={"message": f"Connected to repository: {repository.name}", "session_id": session_id}, status_code=status_code)
+        response.set_cookie(
+            key="sessionID",
+            value=session_id,
+            httponly=True,
+            samesite="None",
+            secure=True
+        )
+        return response
+    
+    return JSONResponse(content={"message": f"Failed to find repository: {repository.name}. Please try again."}, status_code=status_code)
+
+@app.post("/disconnect")
+def disconnect_from_repo(sessionID: str = Cookie(None)):
+    if not sessionID:
+        return JSONResponse(content={"error": "Session ID missing"}, status_code=400)
+    
+    redis_client.delete(sessionID)
+
+    response = JSONResponse(content={"message": "Disconnected from repository"}, status_code=200)
+    response.delete_cookie("sessionID")
+    return response
+
+
+@app.post("/query")
+def query_repo(q: Question, sessionID: str = Cookie(None)):
+    if not llm_client:
+        return JSONResponse(content={"error": "LLM client not initialized"}, status_code=500)
+    
+    if not sessionID:
+        return JSONResponse(content={"error": "Session ID missing"}, status_code=400)
+    
+    context = redis_client.get(sessionID)    
+    answer = llm_client.query(context, q.text)
+    return JSONResponse(content={"answer": answer}, status_code=200)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
